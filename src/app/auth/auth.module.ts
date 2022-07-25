46d7c93fba2662d804607d7c05475b98a8656d59
manueladinizc <manuela.mdc@gmail.com>
2022-07-25 13:36:28 -0300

    recaptcha

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,6 +5,7 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { MaterialModule } from '../material/material.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { NgxCaptchaModule } from 'ngx-captcha';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { ReactiveFormsModule } from '@angular/forms';
     CommonModule,
     AuthRoutingModule,
     MaterialModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxCaptchaModule
   ]
 })
 export class AuthModule { }
